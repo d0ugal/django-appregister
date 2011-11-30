74aff67ece7fbdf246702511a131f8d2065befd8
Dougal Matthews <dougal85@gmail.com>
2011-11-30 10:38:49 +0000

    Added a comment to explian the file, and why its there to create an import error

--- tests/test_appregister/questions_error.py
+++ tests/test_appregister/questions_error.py
@@ -1,1 +1,5 @@
-from fake_modle_name import something
+# This file, on purpose, raises an ImportError. It is used in the tests to
+# verify that ImportErrors are not lost (by mistaking the import error for this
+# file not existing)
+
+from fake_module_name import something
