5441b82eb26ac47b0446ff265d031bda1c16c5ce
Dougal Matthews <dougal85@gmail.com>
2012-06-19 20:16:32 +0100

    Version bump for 0.4 dev.

--- appregister/__init__.py
+++ appregister/__init__.py
@@ -1,5 +1,5 @@
 # following PEP 386, versiontools will pick it up
-__version__ = (0, 3, 0, "final", 0)
+__version__ = (0, 4, 0, "dev", 0)
 
 from appregister.base import Registry, NamedRegistry, SortedRegistry
 
